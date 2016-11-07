09564e3c5629f570d9b29e5ae8c8717251765756
Ricardo Fontana <fontanaricardo@gmail.com>
2016-11-07 16:13:09 -0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(RunCommand());
+        }
+
+        public static bool RunCommand()
+        {
+            try
+            {
+                var procStartInfo = new ProcessStartInfo("cal")
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = false
+                };
+
+                var proc = new Process { StartInfo = procStartInfo };
+                proc.Start();
+
+                // Get the output into a string
+                var output = proc.StandardOutput.ReadToEnd();
+
+                return proc.ExitCode == decimal.Zero ? true : false;
+            }
+            finally
+            {
+                // do something
+            }
+        }
+    }
+}
